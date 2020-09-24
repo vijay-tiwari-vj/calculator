ec20171338391e9a81311b1e6343ed5c1fb5df44
Vijay Tiwari <vijay.tiwari.vj7>
2020-09-24 19:15:02 +0530

    Updates

---
+++ precache-manifest.3614427d73139bda28206c96854c138b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34ba537ccbdcb7d8547bf927e1c8f1c",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e496d072b17cfb5e7b0f",
+    "url": "/calculator/static/css/main.787eab96.chunk.css"
+  },
+  {
+    "revision": "83c708f55d771b07501a",
+    "url": "/calculator/static/js/2.028d59cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.028d59cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e496d072b17cfb5e7b0f",
+    "url": "/calculator/static/js/main.cf2ec56e.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
